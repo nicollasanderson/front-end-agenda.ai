ee84147e078b9a46017eaae468c580e797b3ed35
Nicollas Anderson <nicollas_sdias@hotmail.com>
2023-02-20 14:44:53 -0300

    feat: alteração no link de produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const app = axios.create({
-  // baseURL: "https://agendaai-api-production.up.railway.app/api/",
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://agendaai-api-production.up.railway.app/api/",
+  // baseURL: "http://localhost:8000/api",
 });
 
 export default app;
